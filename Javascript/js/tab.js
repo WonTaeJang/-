05710b818fc157913d1e0833b9cd5a761a881959
wontaejang <jwt0212@gmail.com>
2021-12-14 10:51:56 +0900

    javascript tab

--- Javascript/js/tab.js
+++ Javascript/js/tab.js
@@ -1,5 +1,14 @@
 for(let i = 0; i < $('.tab-button').length; i++)
 {
+    //tabOpen(i);
+}
+
+// javascript
+$('.list').click(function(e){
+    tabOpen(e.target.dataset.id);
+});
+
+function tabOpen(i){
     $('.list .tab-button').eq(i).click(function(){
         $('.list .tab-button').removeClass('active');
         $('.tab-content').removeClass('show');
@@ -8,5 +17,3 @@ for(let i = 0; i < $('.tab-button').length; i++)
         $('.tab-content').eq(i).addClass('show');
     })
 }
-
-
