3eacf086f402af68394496e90248a040bed633f9
wontaejang <jwt0212@gmail.com>
2022-01-21 10:46:45 +0900

    js es6 2d object 충돌 체크

--- Javascript ES6/main.js
+++ Javascript ES6/main.js
@@ -3,6 +3,12 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
 
+var img1 = new Image();
+img1.src = '../Image/cactus.png';
+
+var img2 = new Image();
+img2.src = '../Image/dinosaur.png';
+
 var dino = {
     x : 10,
     y : 200,
@@ -10,7 +16,8 @@ var dino = {
     height : 50,
     draw(){
         ctx.fillStyle = 'green';
-        ctx.fillRect(this.x,this.y,this.width,this.height);
+        //ctx.fillRect(this.x,this.y,this.width,this.height);
+        ctx.drawImage(img2, this.x, this.y);
     }
 }
 
@@ -23,23 +30,25 @@ class Cactus{
     }
     draw(){
         ctx.fillStyle = 'red';
-        ctx.fillRect(this.x,this.y,this.width,this.height);
+        //ctx.fillRect(this.x,this.y,this.width,this.height);
+        ctx.drawImage(img1, this.x, this.y);
     }
 }
 
 var timer = 0; 
 var cactus_array = [];
 var 점프timer = 0;
+var animation;
 
 function 프레임마다실행(){
-    requestAnimationFrame(프레임마다실행);
+    animation = requestAnimationFrame(프레임마다실행);
     timer++;
 
     ctx.clearRect(0,0,canvas.width, canvas.height)
 
     // 144프레임마다 장애물 소환 후 array에 보관
     // 필요없어진 장애물은 제거
-    if(timer % 120 === 0){
+    if(timer % 200 === 0){
         var cactus = new Cactus();
         cactus_array.push(cactus);
     }
@@ -51,7 +60,7 @@ function 프레임마다실행(){
             o.splice(i,1);
         }
 
-        
+        충돌확인(dino, a);
 
         a.x--;
         a.draw();
@@ -78,6 +87,18 @@ function 프레임마다실행(){
 }
 
 프레임마다실행();
+
+// 충돌 확인 
+function 충돌확인(dino, cactus){
+    var x축차이 = cactus.x - (dino.x + dino.width);
+    var y축차이 = cactus.y - (dino.y + dino.height);
+
+    if(x축차이 < 0 && y축차이 < 0){
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        cancelAnimationFrame(animation);
+    }
+}
+
 
 var 점프중 = false;
 
