828322588ccf6d94a995bdf0f802219854913bd0
WonTaeJang <jwt0212@gmail.com>
2022-01-30 22:42:54 +0900

    nodejs mongodb post insert

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -4,11 +4,19 @@ const bodyParser = require('body-parser');
 app.use(express.urlencoded({extended: true})) 
 app.use(bodyParser.urlencoded({extended: true})) 
 
+var db;
 const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb+srv://mongoTest:!Q2w3e4r@cluster0.31lzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',function(err, client){
     if(err){
         return console.log(err);
     }    
+
+    // todoapp이라는 database 폴더
+    db = client.db('todoapp');
+
+    // db.collection('post').insertOne({이름 : 'John', 나이 : 20, id:100}, function(_err, result){
+    //     console.log('저장완료');
+    // });
 
     app.listen(8080, function(){
         console.log('listening on 8080');
@@ -25,7 +33,12 @@ app.get('/write', function(req,res){
 
 app.post('/add', function(req, res){
     res.send('전송완료')
-    console.log(req.body);
+    console.log(req.body.data);
+    console.log(req.body.title);
+
+    db.collection('post').insertOne(req.body, function(_err, result){
+        console.log('저장완료,' + result);
+    });
 });
 
 app.get('/beauty', function(req,res){
