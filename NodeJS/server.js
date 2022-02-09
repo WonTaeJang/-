d318c235ae673145077ee4c05260c564c75d676a
WonTaeJang <jwt0212@gmail.com>
2022-02-09 22:59:40 +0900

    nodejs passport 1

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(express.urlencoded({extended: true})) 
-app.use(bodyParser.urlencoded({extended: true})) 
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 var db;
 const MongoClient = require('mongodb').MongoClient;
 
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 // static 폴더
 app.use('/public', express.static('public'));
 
-MongoClient.connect('mongodb+srv://mongoTest:!Q2w3e4r@cluster0.31lzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',function(err, client){
-    if(err){
+MongoClient.connect('mongodb+srv://mongoTest:!Q2w3e4r@cluster0.31lzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function (err, client) {
+    if (err) {
         return console.log(err);
-    }    
+    }
 
     // todoapp이라는 database 폴더
     db = client.db('todoapp');
@@ -26,41 +26,41 @@ MongoClient.connect('mongodb+srv://mongoTest:!Q2w3e4r@cluster0.31lzr.mongodb.net
     //     console.log('저장완료');
     // });
 
-    app.listen(8080, function(){
+    app.listen(8080, function () {
         console.log('listening on 8080');
     });
 })
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     //res.sendFile(__dirname + '/html/index.html');
 
-    db.collection('post').find().toArray(function(err, result){
+    db.collection('post').find().toArray(function (err, result) {
         //console.log(result)
-        res.render('list.ejs', {posts : result});
+        res.render('list.ejs', { posts: result });
     })
 });
 
-app.get('/index', function(req,res){
+app.get('/index', function (req, res) {
     res.render('index.ejs');
 });
 
 // create
-app.get('/write', function(req,res){
+app.get('/write', function (req, res) {
     //res.sendFile(__dirname + '/html/write.html');
     res.render('write.ejs');
 });
 
 // update
-app.get('/edit/:id', function(req,res){
-    db.collection('post').findOne({_id : parseInt(req.params.id)}, function(err, result){
+app.get('/edit/:id', function (req, res) {
+    db.collection('post').findOne({ _id: parseInt(req.params.id) }, function (err, result) {
         //console.log(result);
 
-        if(err){
+        if (err) {
             res.status(400);
-            return ;
+            return;
         }
 
-        res.render('edit.ejs', {data : result}) 
+        res.render('edit.ejs', { data: result })
     })
 });
 
@@ -84,20 +84,20 @@ app.put('/edit', function (req, res) {
         });
 });
 
-app.post('/add', function(req, res){
+app.post('/add', function (req, res) {
     res.send('requset success')
     // console.log(req.body.data);
     // console.log(req.body.title);
 
     // 글 번호 달기
     // 유니크한 글번호를 달기 
-    db.collection('counter').findOne({name : '게시물갯수'}, function(err, result){
-        if(err){return console.log(err)}
+    db.collection('counter').findOne({ name: '게시물갯수' }, function (err, result) {
+        if (err) { return console.log(err) }
 
         //console.log(result.totalPost);
         var 총게시물갯수 = result.totalPost;
 
-        db.collection('post').insertOne({_id : 총게시물갯수 + 1 , 제목: req.body.title, 날짜 : req.body.date} , function(_err, result){
+        db.collection('post').insertOne({ _id: 총게시물갯수 + 1, 제목: req.body.title, 날짜: req.body.date }, function (_err, result) {
             console.log('record success');
 
             // 총게시물갯수 증감 
@@ -105,8 +105,8 @@ app.post('/add', function(req, res){
             //db.collection('counter').updateOne({어떤데이터를 수정할지},{수정 값}, function(){})
             // operator set: {$set : {key: 바꿀 값}}
             // operator set: {$inc : {key: 기존값에 더해줄 값}}
-            db.collection('counter').updateOne({name: '게시물갯수'},{$inc : {totalPost : 1} }, function(err, result){
-                if(err){return console.log(err)}
+            db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalPost: 1 } }, function (err, result) {
+                if (err) { return console.log(err) }
                 console.log('db counter update success')
             });
 
@@ -115,48 +115,85 @@ app.post('/add', function(req, res){
 });
 
 // ejs는 views 폴더안에서 작성해야한다.
-app.get('/list', function(req, res){
-    db.collection('post').find().toArray(function(err, result){
+app.get('/list', function (req, res) {
+    db.collection('post').find().toArray(function (err, result) {
         //console.log(result)
-        res.render('list.ejs', {posts : result});
+        res.render('list.ejs', { posts: result });
     })
 });
 
-app.get('/beauty', function(req,res){
+app.get('/beauty', function (req, res) {
     res.send('뷰티용품 쇼핑 페이지임.')
 });
 
-app.delete('/delete', function(req, res){
+app.delete('/delete', function (req, res) {
     console.log(req.body);
 
     // string to int
-    req.body._id = parseInt(req.body._id); 
+    req.body._id = parseInt(req.body._id);
 
     //db.collection('post').deleteOne({}, function(){})
-    db.collection('post').deleteOne(req.body, function(err, result){
-        if(err){
+    db.collection('post').deleteOne(req.body, function (err, result) {
+        if (err) {
             res.status(400);
             return console.log(err)
         }
 
         console.log('db counter delete success')
-        res.status(200).send({message : '성공했습니다.'});
+        res.status(200).send({ message: '성공했습니다.' });
     });
 });
 
 // default/?? parameter로 요청가능한 URL 만들기
-app.get('/detail/:id', function(req, res){
-    db.collection('post').findOne({_id : parseInt(req.params.id)}, function(err, result){
+app.get('/detail/:id', function (req, res) {
+    db.collection('post').findOne({ _id: parseInt(req.params.id) }, function (err, result) {
         console.log(result);
 
-        if(err){
+        if (err) {
             res.status(400);
-            return ;
+            return;
         }
 
-        res.render('detail.ejs', {data : result}) 
+        res.render('detail.ejs', { data: result })
     })
 
-    
+
 });
 
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function (req, res) {
+    res.render('login.ejs');
+})
+
+app.post('/login', passport.authenticate('local', {
+    failureRedirect: '/fail'
+}), function (req, res) {
+    res.redirect('/');
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+    //console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+        if (에러) return done(에러)
+
+        if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+        if (입력한비번 == 결과.pw) {
+            return done(null, 결과)
+        } else {
+            return done(null, false, { message: '비번틀렸어요' })
+        }
+    })
+}));
