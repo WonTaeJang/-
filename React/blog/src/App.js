e39440a3695f37be7bac05061c6da9fa2c22cc45
wontaejang <jwt0212@gmail.com>
2022-03-10 16:51:10 +0900

    react state 변경하는법

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -6,15 +6,14 @@ import './App.css';
 // 리액트에서 데이터 바인딩을 쉽게 할 수 있다.
 function App() {
   let posts = '강남 고기 맛집';
+  let [따봉, 따봉변경] = useState(0);
 
   // state: 
   // 1. 변수 대신 쓰는 데이터 저장공간
   // 2. useState()를 이용해 만들어야함
   // 3. 문자, 숫자, array object 다 저장가능
   // state에 데이터 저장해놓은 이유 : state는 변경되면 HTML이 자동으로 재렌더링이 됩니다.
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);
-  let [글제목2, 글제목변경2] = useState('여자 코트 추천');
-  let [글제목3, 글제목변경3] = useState('공용 코트 추천');
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '여자 코트 추천','공용 코트 추천']);
 
   return (
     <div className="App">
@@ -22,6 +21,12 @@ function App() {
         {/* style은 object 방식으로 넣어야한다. */}
         {/* camelCase로 '-'는 뺄셈으로 인식하기 때문에 뒤단어를 대문자로 써야한다. */}
         <div>개발 Blog</div>
+      </div>
+      <button onClick={()=>{글제목변경(['여자 코트 추천', 글제목[1], 글제목[2]])}}>버튼</button>
+      <div className="list">
+        <h3>{글제목[0]} <span onClick={()=>{따봉변경(따봉 + 1)}}>❤</span> {따봉} </h3>
+        <p>2월 17일 발행</p>
+        <hr></hr>
       </div>
 
       <div className="list">
@@ -31,13 +36,7 @@ function App() {
       </div>
 
       <div className="list">
-        <h3>{글제목2}</h3>
-        <p>2월 17일 발행</p>
-        <hr></hr>
-      </div>
-
-      <div className="list">
-        <h3>{글제목3}</h3>
+        <h3>{글제목[2]}</h3>
         <p>2월 17일 발행</p>
         <hr></hr>
       </div>
