29a445e21a06ffc8136bdc0ea5bcbb5a68d06154
wontaejang <jwt0212@gmail.com>
2022-03-17 11:00:03 +0900

    react 레이아웃 디자인

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -1,11 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <button type="button" className="btn btn-primary">Primary</button>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="jumbotron background">
+        <h1 className="display-4">20% Season Off</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4" />
+        <p className="lead">
+          <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </p>
+      </div>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="100%"/>
+            <h4>상품명</h4>
+            <p>상품설명 & 정보</p>
+          </div>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="100%"/>
+            <h4>상품명</h4>
+            <p>상품설명 & 정보</p>
+          </div>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="100%"/>
+            <h4>상품명</h4>
+            <p>상품설명 & 정보</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
