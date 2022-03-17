17b538a74739b661e1b6c4e1aeb13007078b62fd
wontaejang <jwt0212@gmail.com>
2022-03-17 10:29:36 +0900

    react 쇼핑몰 사이트 시작, bootstrap 세팅

---
+++ React/shop/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" className="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
