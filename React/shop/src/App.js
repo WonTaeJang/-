518d6fb090fdd519a463cbbcb1f8e5277d1231b5
WonTaeJang <jwt0212@gmail.com>
2022-03-19 23:41:54 +0900

    React history, switch

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import { useState } from 'react';
 import data from './data.js';
+import Detail from './Detail.js';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -19,8 +20,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link ><Link to="/">Home</Link></Nav.Link>
+              <Nav.Link ><Link to="/detail">Detail</Link></Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -33,69 +34,50 @@ function App() {
         </Container>
       </Navbar>
 
-      <Route exact path='/'>
-        <div className="jumbotron background">
-          <h1 className="display-4">20% Season Off</h1>
-          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-          <hr className="my-4" />
-          <p className="lead">
-            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-          </p>
-        </div>
+      {/* 스위치는 중복을 허용하지 않는다. */}
+      <Switch>
+        <Route exact path='/'>
+          <div className="jumbotron background">
+            <h1 className="display-4">20% Season Off</h1>
+            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr className="my-4" />
+            <p className="lead">
+              <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </p>
+          </div>
 
-        <div className='container'>
-          <div className='row'>
-            {
-              shoes.map(function (신발, i) {
-                return (
-                  // <Card shoes={shoes[i]}></Card>
-                  <Card shoes={신발} i={i} key={i}></Card>
-                )
-              })
-            }
-            {/* <Products shoes={shoes[0]}></Products>
-          <Products shoes={shoes[1]}></Products>
-          <Products shoes={shoes[2]}></Products> */}
-
-            {/* <div className='col-md-4'>
-            <img src={shoes[0].img_url} width="100%"/>
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].content}</p>
-            <p>{shoes[0].price}</p>
-          </div>
-          <div className='col-md-4'>
-            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="100%"/>
-            <h4>상품명</h4>
-            <p>상품설명 & 정보</p>
-          </div>
-          <div className='col-md-4'>
-            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="100%"/>
-            <h4>상품명</h4>
-            <p>상품설명 & 정보</p>
-          </div> */}
-          </div>
-        </div>
-      </Route>
-      <Route path='/detail'>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button>
+          <div className='container'>
+            <div className='row'>
+              {
+                shoes.map(function (신발, i) {
+                  return (
+                    // <Card shoes={shoes[i]}></Card>
+                    <Card shoes={신발} i={i} key={i}></Card>
+                  )
+                })
+              }
             </div>
           </div>
-        </div>
-      </Route>
+        </Route>
+        <Route path='/detail'>
+          <Detail></Detail>
+        </Route>
+
+        <Route path="/:id">
+          <div>
+            아무거나 적어보자
+          </div>
+        </Route>
+      </Switch>
+
+
 
 
     </div>
   );
 }
+
+
 
 function Card(Props) {
   return (
