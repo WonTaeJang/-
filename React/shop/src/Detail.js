5bac56922c2783bbb077a3d19def8708969482e4
WonTaeJang <jwt0212@gmail.com>
2022-03-20 18:32:32 +0900

    React styled-components

--- React/shop/src/Detail.js
+++ React/shop/src/Detail.js
@@ -1,5 +1,17 @@
 import React, { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+//  styled-components
+// CSS를 미리 입혀놓은 컴포넌트
+let 박스 = styled.div`
+  padding : 20px;
+`;
+
+let 제목 = styled.h4`
+  font-size : 25px;
+  color : ${ props => props.색상 }
+`
 
 function Detail(props) {
   let history = useHistory();
@@ -11,6 +23,11 @@ function Detail(props) {
 
   return (
     <div className="container">
+
+      <박스>
+        <제목 색상={'red'}>상세 페이지</제목>
+      </박스>
+
       <div className="row">
         <div className="col-md-6">
           <img src={`https://codingapple1.github.io/shop/shoes${(찾은상품.id + 1)}.jpg`} width="100%" />
