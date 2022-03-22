614c678e4f48be2aee5feadce5ee96e71de92bab
wontaejang <jwt0212@gmail.com>
2022-03-22 11:57:54 +0900

    React useEffect

--- React/shop/src/Detail.js
+++ React/shop/src/Detail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss'
@@ -14,7 +14,38 @@ let 제목 = styled.h4`
   color : ${ props => props.색상 }
 `
 
+// Hook: Component Lifecycle 중간중간에 뭔가 명령을 줄 수 있음
+class Detail2 extends React.Component {
+  // Detail2 컴포넌트가 Mount 되었을 때 실행할 코드
+  componentDidMount(){
+
+  }
+
+  // Detail2 컴포넌트가 해제(Unmount)될때 실행할 코드
+  componentWillUnmount(){
+
+  }
+}
+
 function Detail(props) {
+
+  let [visible, setVisible] = useState({visibility:'none'});
+
+  // 컴포넌트가 mount 되었을 때, 컴포넌트가 update 되었을 때 
+  // useEffect는 여러개 작성해도 되고 적은 순서대로 실행된다.
+  useEffect(()=>{
+    console.log(11111)
+    // 2초후에 alert창을 사라지게 하기
+    //console.log(visible);
+    let 타이머 = setTimeout(()=>{
+      setVisible({visibility:'hidden'});
+    },2000);
+
+    // Detail function이 사라질때 실행되는 코드
+    return ()=>{}
+  });
+
+
   let history = useHistory();
   let { id } = useParams();
 
@@ -29,9 +60,7 @@ function Detail(props) {
         <제목 className='red'>상세 페이지</제목>
       </박스>
 
-      <div className='my-alert2'>
-        <p>재고가 얼마 남지 않았습니다.</p>
-      </div>
+      <Alert vb={visible}></Alert>
 
       <div className="row">
         <div className="col-md-6">
@@ -49,6 +78,14 @@ function Detail(props) {
       </div>
     </div>
   )
+
+  function Alert(props) {
+    return (
+      <div className='my-alert2' style={props.vb}>
+        <p>재고가 얼마 남지 않았습니다.</p>
+      </div>
+    )
+  }
 }
 
 export default Detail;
