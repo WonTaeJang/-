caee0b5508cd600ea7d5fa2ebad7b141d50787f6
WonTaeJang <jwt0212@gmail.com>
2022-04-29 18:37:14 +0900

    typescript narrowing & assertion

---
+++ Typescript/day3-narrowing.js
@@ -0,0 +1,13 @@
+function 내함수(x) {
+    if (typeof x === 'string')
+        return x + '1';
+    var array = [];
+    if (typeof x === 'number')
+        array[0] = x;
+    // assertion 문법 (타입 덮어쓰기)
+    // assertion은 narrowing에 사용
+    // assertion은 유니온 타입을 하나로 확정할때 사용
+    // old assertion: <number> 변수
+    array[0] = x;
+}
+내함수(123);
