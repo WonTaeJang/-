e3e828ad2a35c29c345d3cafcc87889c549c01da
WonTaeJang <jwt0212@gmail.com>
2022-05-18 23:31:53 +0900

    typescript + react, Redux tookit

--- Typescript/react/myapp/src/App.tsx
+++ Typescript/react/myapp/src/App.tsx
@@ -1,22 +1,21 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {RootState, 증가} from './index';
+import {Dispatch} from 'redux';
+
 
 function App() {
-  let [user, setUser] = useState(['kim']);
+  const 꺼내온거 = useSelector( (state :RootState) => state );
+  const dispatch :Dispatch = useDispatch();
 
   return (
     <div className="App">
-      <div>안녕하십니까</div>
-      <Profile name='철수' age='20'></Profile>
+      { 꺼내온거.counter1.count }
+      <button onClick={()=>{dispatch(증가())}}>버튼</button>
     </div>
   );
-}
-
-function Profile(props:{name:string, age:String}) :JSX.Element {
-  return (
-    <div>{props.name}프로필입니다.</div>
-  )
 }
 
 export default App;
--- Typescript/react/myapp/src/index.tsx
+++ Typescript/react/myapp/src/index.tsx
@@ -4,6 +4,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createSlice, configureStore, PayloadAction} from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+const 초기값  = { count: 0, user : 'kim' };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState : 초기값,
+  reducers: {
+    증가 (state){
+      state.count += 1
+    },
+    감소 (state){
+      state.count -= 1
+    },
+    맘대로증가 (state, action :PayloadAction<number>){
+      state.count += action.payload
+    }
+  }
+})
+
+let store = configureStore({
+  reducer: {
+    counter1 : counterSlice.reducer
+  }
+})
+
+//state 타입을 export 해두는건데 나중에 쓸 데가 있음
+export type RootState = ReturnType<typeof store.getState>
+
+export let {증가, 감소, 맘대로증가} = counterSlice.actions;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
