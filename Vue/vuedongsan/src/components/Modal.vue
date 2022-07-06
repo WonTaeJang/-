5011f43fb1fcae9bbb47c2e2b8d9df723b217cfb
WonTaeJang <jwt0212@gmail.com>
2022-07-06 22:52:55 +0900

    vue watcher 적용하여 문자입력 방지

--- Vue/vuedongsan/src/components/Modal.vue
+++ Vue/vuedongsan/src/components/Modal.vue
@@ -25,6 +25,17 @@ export default {
             month : 1,
         }
     },
+    watch : {
+        // 데이터 감시하려면 감시할데이터(){}
+        // month라는 데이터가 변할때마다 watcher도 실행됨
+        month(a){
+            // 사용자가 month를 글자로 입력하면 경고문 띄워주기
+            if(isNaN(a)){
+                alert('문자를 입력하면 안됩니다.');
+                this.month = 1;
+            }
+        }
+    },
     props : {
         원룸들 : Object,
         누른거 : Number,
