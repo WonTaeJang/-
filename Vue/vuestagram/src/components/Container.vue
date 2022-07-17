f4cb8b82f6fcd8b2368b0a52a29507b06a2050f4
WonTaeJang <jwt0212@gmail.com>
2022-07-17 12:30:39 +0900

    vuestagram start

---
+++ Vue/vuestagram/src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Post v-for="(e,i) in story" :key="i" v-bind:story='e'/>
+    </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+    //eslint-disable-next-line
+    name : 'container',
+    components : {
+        Post : Post,
+    },
+    props : {
+        story : Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
